70834d1959eb787c799ad3ecd9f4a8647234303e
hyperfield <sequence@myrealm.ca>
2025-07-24 09:28:04 +0200

    fix(filescanner): bug fix, skip junk system files

--- app/include/FileScanner.hpp
+++ app/include/FileScanner.hpp
@@ -17,6 +17,7 @@ public:
 
 private:
     bool is_file_hidden(const fs::path &path);
+    bool is_junk_file(const std::string& name);
 };
 
 #endif
--- app/lib/FileScanner.cpp
+++ app/lib/FileScanner.cpp
@@ -1,6 +1,7 @@
 #include "FileScanner.hpp"
 #include <iostream>
 #include <filesystem>
+#include <unordered_set>
 #include <gtk/gtk.h>
 
 #ifdef _WIN32
@@ -9,30 +10,40 @@
 
 namespace fs = std::filesystem;
 
+
 std::vector<FileEntry>
 FileScanner::get_directory_entries(const std::string &directory_path,
                                    FileScanOptions options)
 {
     std::vector<FileEntry> file_paths_and_names;
+
     for (const auto &entry : fs::directory_iterator(directory_path)) {
         std::string full_path = entry.path().string();
         std::string file_name = entry.path().filename().string();
+        bool is_hidden = is_file_hidden(full_path);
+        bool should_add = false;
         FileType file_type;
 
-        bool is_hidden = is_file_hidden(full_path);
+        if (is_junk_file(file_name)) continue;
 
         if (has_flag(options, FileScanOptions::Files) &&
             fs::is_regular_file(entry)) {
             if (has_flag(options, FileScanOptions::HiddenFiles) || !is_hidden) {
                 file_type = FileType::File;
-            }
-        } else if (has_flag(options, FileScanOptions::Directories) &&
-            fs::is_directory(entry)) {
-            if (has_flag(options, FileScanOptions::HiddenFiles) || !is_hidden) {
-                file_type = FileType::Directory;
+                should_add = true;
             }
         }
-        file_paths_and_names.push_back({full_path, file_name, file_type});
+        else if (has_flag(options, FileScanOptions::Directories) &&
+                 fs::is_directory(entry)) {
+            if (has_flag(options, FileScanOptions::HiddenFiles) || !is_hidden) {
+                file_type = FileType::Directory;
+                should_add = true;
+            }
+        }
+
+        if (should_add) {
+            file_paths_and_names.push_back({full_path, file_name, file_type});
+        }
     }
 
     return file_paths_and_names;
@@ -40,10 +51,19 @@ FileScanner::get_directory_entries(const std::string &directory_path,
 
 
 bool FileScanner::is_file_hidden(const fs::path &path) {
-    #ifdef _WIN32
+#ifdef _WIN32
     DWORD attrs = GetFileAttributesW(path.c_str());
     return (attrs != INVALID_FILE_ATTRIBUTES) &&
-    (attrs & FILE_ATTRIBUTE_HIDDEN);
-    #endif
-    return path.string().starts_with(".");
+           (attrs & FILE_ATTRIBUTE_HIDDEN);
+#else
+    return path.filename().string().starts_with(".");
+#endif
+}
+
+
+bool FileScanner::is_junk_file(const std::string& name) {
+    static const std::unordered_set<std::string> junk = {
+        ".DS_Store", "Thumbs.db", "desktop.ini"
+    };
+    return junk.contains(name);
 }
